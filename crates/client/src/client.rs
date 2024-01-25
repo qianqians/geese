82f33af879567e505a7199487721ec859bf8a09b
qianqians <451517996@qq.com>
2024-01-25 16:52:47 +0800

    client lock

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -110,9 +110,15 @@ impl GateMsgHandle {
         };
 
         let _proxy_clone = _proxy.clone();
-        let mut _p = _proxy.as_ref().lock().unwrap();
-        println!("do_client_event _proxy lock!");
-        let mut _handle = _p.msg_handle.as_ref().lock().unwrap();
+
+        let _msg_handle:Arc<StdMutex<GateMsgHandle>>;
+        {
+            let mut _p = _proxy.as_ref().lock().unwrap();
+            println!("do_client_event _proxy lock!");
+            _msg_handle = _p.msg_handle.clone();
+        }
+
+        let mut _handle = _msg_handle.as_ref().lock().unwrap();
         println!("do_client_event msg_handle lock!");
         _handle.enque_event(ConnEvent{
             gate_proxy: Arc::downgrade(&_proxy_clone),
