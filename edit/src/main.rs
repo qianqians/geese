3b8702fd46ef1e418092daade5a4f05f8db58b86
qianqians <451517996@qq.com>
2024-02-24 16:37:15 +0800

    edit main

--- edit/src/main.rs
+++ edit/src/main.rs
@@ -6,7 +6,6 @@ use winit::window::Icon;
 
 struct Images {
     bevy_icon: Handle<Image>,
-    bevy_icon_inverted: Handle<Image>,
 }
 
 impl FromWorld for Images {
@@ -14,7 +13,6 @@ impl FromWorld for Images {
         let asset_server = world.get_resource_mut::<AssetServer>().unwrap();
         Self {
             bevy_icon: asset_server.load("icon.png"),
-            bevy_icon_inverted: asset_server.load("icon_inverted.png"),
         }
     }
 }
@@ -49,11 +47,6 @@ fn main() {
 
 #[derive(Default, Resource)]
 struct UiState {
-    pub label: String,
-    pub value1: f32,
-    pub painting: Painting,
-    pub inverted: bool,
-    pub egui_texture_handle: Option<egui::TextureHandle>,
     pub is_window_open: bool,
 }
 
@@ -128,32 +121,11 @@ fn setup_system(
 }
 
 fn ui_example_system(
-    mut ui_state: ResMut<UiState>,
-    // You are not required to store Egui texture ids in systems. We store this one here just to
-    // demonstrate that rendering by using a texture id of a removed image is handled without
-    // making bevy_egui panic.
     mut rendered_texture_id: Local<egui::TextureId>,
     mut is_initialized: Local<bool>,
-    // If you need to access the ids from multiple systems, you can also initialize the `Images`
-    // resource while building the app and use `Res<Images>` instead.
     images: Local<Images>,
     mut contexts: EguiContexts,
 ) {
-    let egui_texture_handle = ui_state
-        .egui_texture_handle
-        .get_or_insert_with(|| {
-            contexts.ctx_mut().load_texture(
-                "example-image",
-                egui::ColorImage::example(),
-                Default::default(),
-            )
-        })
-        .clone();
-
-    let mut load = false;
-    let mut remove = false;
-    let mut invert = false;
-
     if !*is_initialized {
         *is_initialized = true;
         *rendered_texture_id = contexts.add_image(images.bevy_icon.clone_weak());
@@ -230,20 +202,4 @@ fn ui_example_system(
             ui.heading("Side Panel Left");
             ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
         });
-
-    if invert {
-        ui_state.inverted = !ui_state.inverted;
-    }
-    if load || invert {
-        // If an image is already added to the context, it'll return an existing texture id.
-        if ui_state.inverted {
-            *rendered_texture_id = contexts.add_image(images.bevy_icon_inverted.clone_weak());
-        } else {
-            *rendered_texture_id = contexts.add_image(images.bevy_icon.clone_weak());
-        };
-    }
-    if remove {
-        contexts.remove_image(&images.bevy_icon);
-        contexts.remove_image(&images.bevy_icon_inverted);
-    }
 }
