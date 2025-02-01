6344e03b5ed72d824d41ab9b428b1ec59651a9fa
吴铭 <451517996@qq.com>
2025-02-01 12:43:43 +0800

    group remove

--- sample/server/src/engine/engine/group.py
+++ sample/server/src/engine/engine/group.py
@@ -24,7 +24,7 @@ class group(object):
             if cli_gate_name == gate_name and cli_conn_id == conn_id:
                 self.clients.remove(_c)
                 break
-
+    
     def create_remote_entity(self, _e:entity):
         self.entities[_e.entity_id] = _e
         gate_clients:dict[str, list[str]] = {}
@@ -38,6 +38,22 @@ class group(object):
             gate_name, list_conn_id = _conn
             _e.create_remote_entity(gate_name, list_conn_id)
 
+    def remove_entity(self, _e:entity):   
+        gate_clients:dict[str, list[str]] = {}
+        for _c in self.clients:
+            gate_name, conn_id = _c
+            if gate_name not in gate_clients:
+                gate_clients[gate_name] = [conn_id]
+            else:
+                gate_clients[gate_name].append(conn_id)
+
+        for _conn in gate_clients.items():
+            gate_name, list_conn_id = _conn
+            from app import app
+            app().ctx.hub_call_client_delete_remote_entity(gate_name, _e.entity_id)
+
+        del self.entities[_e.entity_id]
+        
     def create_remote_player(self, _p:player):
         self.players[_p.entity_id] = _p
         gate_clients:dict[str, list[str]] = {}
@@ -54,5 +70,19 @@ class group(object):
             _p.create_remote_entity(gate_name, list_conn_id)
         _p.create_main_remote_entity()
         
-            
+    def remove_player(self, _p:player):   
+        gate_clients:dict[str, list[str]] = {}
+        for _c in self.clients:
+            gate_name, conn_id = _c
+            if gate_name not in gate_clients:
+                gate_clients[gate_name] = [conn_id]
+            else:
+                gate_clients[gate_name].append(conn_id)
+
+        for _conn in gate_clients.items():
+            gate_name, list_conn_id = _conn
+            from app import app
+            app().ctx.hub_call_client_delete_remote_entity(gate_name, _p.entity_id)
+
+        del self.players[_p.entity_id]
 
