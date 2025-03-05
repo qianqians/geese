e889041513d5689be1cfcd41028eed2e4b583610
吴铭 <451517996@qq.com>
2025-03-06 00:11:46 +0800

    transfer_timeout

--- server/engine/context.py
+++ server/engine/context.py
@@ -3,6 +3,13 @@ from threading import Timer
 from collections.abc import Callable
 from .pyhub import HubContext
 
+def transfer_timeout(new_gate_name:str, new_conn_id:str, sdk_uuid:str, token:str):
+    from app import app
+    _t = app().ctx.transfer_timeout[new_conn_id]
+    if _t!= None:
+        app().ctx.transfer_timeout.pop(new_conn_id)
+        app().login_handle.reconnect(new_gate_name, new_conn_id, sdk_uuid, token)
+
 class context(object):
     def __init__(self, cfg_file:str) -> None:
         self.ctx = HubContext(cfg_file)
@@ -127,7 +134,7 @@ class context(object):
 
     def hub_call_replace_client(self, old_gate_name:str, old_conn_id:str, new_gate_name:str, new_conn_id:str, sdk_uuid:str, token:str, is_replace:bool, prompt_info:str) -> bool:
         from app import app
-        self.transfer_timeout[new_conn_id] = Timer(1000, app().login_handle.reconnect(new_gate_name, new_conn_id, sdk_uuid, token))
+        self.transfer_timeout[new_conn_id] = Timer(1000, lambda : transfer_timeout(new_gate_name, new_conn_id, sdk_uuid, token))
         return self.ctx.hub_call_transfer_client(old_gate_name, old_conn_id, new_gate_name, new_conn_id, is_replace, prompt_info)
     
     def hub_call_gate_wait_migrate_entity(self, gate_name:str, entity_id:str) -> bool:
