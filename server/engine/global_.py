3e8806c796e5d9fba0e97d54d0e7160740fba387
wuming <451517996@qq.com>
2025-06-18 17:48:21 +0800

    fixed bug: singleton global_entity

--- server/engine/global_.py
+++ server/engine/global_.py
@@ -3,14 +3,6 @@ from app import singleton, app
 
 @singleton
 class global_entity(object):
-    __instance__ = None
-    
-    def __init__(self):
-        __instance__ = self
-        
-    def instance() -> app:
-        return app.__instance__
-
     def call_client(self, method:str, argvs:bytes):
         for gate_name in self.conn_client_gate:
             app().ctx.hub_call_client_global(gate_name, method, argvs)
