f033f43754121637fa6b5c2c314c4b9b466ad4a4
wuming <451517996@qq.com>
2025-07-03 17:48:14 +0800

    review

--- server/lib/hub/src/hub_msg_handle.rs
+++ server/lib/hub/src/hub_msg_handle.rs
@@ -8,7 +8,18 @@ use proto::common::RegServer;
 
 // hub msg
 use proto::hub::{
-    CreateServiceEntity, HubCallHubCreateMigrateEntity, HubCallHubErr, HubCallHubMigrateEntity, HubCallHubMigrateEntityComplete, HubCallHubNtf, HubCallHubRpc, HubCallHubRsp, HubCallHubWaitMigrateEntity, HubForwardClientRequestService, HubForwardClientRequestServiceExt, QueryServiceEntity
+    CreateServiceEntity, 
+    HubCallHubCreateMigrateEntity, 
+    HubCallHubErr, 
+    HubCallHubMigrateEntity, 
+    HubCallHubMigrateEntityComplete, 
+    HubCallHubNtf, 
+    HubCallHubRpc, 
+    HubCallHubRsp, 
+    HubCallHubWaitMigrateEntity, 
+    HubForwardClientRequestService, 
+    HubForwardClientRequestServiceExt, 
+    QueryServiceEntity
 };
 
 use crate::hub_service_manager::StdMutex;
@@ -24,8 +35,10 @@ impl HubCallbackMsgHandle {
     pub fn do_reg_hub(&mut self, py: Python<'_>, py_handle: Py<PyAny>, ev: RegServer) {
         trace!("do_rge_hub begin!");
 
-        let hub_name = ev.name.unwrap();
-        let argvs = (hub_name,);
+        let argvs = (
+            ev.name.unwrap(),
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_rge_hub", argvs) {
             error!("do_rge_hub python callback error:{}", e)
         }
@@ -34,7 +47,11 @@ impl HubCallbackMsgHandle {
     pub fn do_query_service_entity(&mut self, py: Python<'_>, py_handle: Py<PyAny>, hub_name: String, ev: QueryServiceEntity) {
         trace!("do_query_service_entity begin!");
 
-        let argvs = (hub_name, ev.service_name.unwrap(),);
+        let argvs = (
+            hub_name, 
+            ev.service_name.unwrap(),
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_query_service_entity", argvs) {
             error!("do_query_service_entity python callback error:{}", e)
         }
@@ -49,7 +66,9 @@ impl HubCallbackMsgHandle {
             ev.service_name.unwrap(), 
             ev.entity_id.unwrap(), 
             ev.entity_type.unwrap(), 
-            PyBytes::new(py, &ev.argvs.unwrap()));
+            PyBytes::new(py, &ev.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_create_service_entity", argvs) {
             error!("do_create_service_entity python callback error:{}", e)
         }
@@ -63,7 +82,9 @@ impl HubCallbackMsgHandle {
             ev.service_name.unwrap(), 
             ev.gate_name.unwrap(), 
             ev.conn_id.unwrap(),
-            ev.argvs.unwrap());
+            ev.argvs.unwrap()
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_forward_client_request_service", argvs) {
             error!("do_forward_client_request_service python callback error:{}", e)
         }
@@ -80,7 +101,9 @@ impl HubCallbackMsgHandle {
         let argvs = (
             hub_name,
             ev.service_name.unwrap(), 
-            request_infos);
+            request_infos
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_forward_client_request_service_ext", argvs) {
             error!("do_forward_client_request_service python callback error:{}", e)
         }
@@ -95,7 +118,9 @@ impl HubCallbackMsgHandle {
             ev.entity_id.unwrap(), 
             ev.msg_cb_id.unwrap(), 
             msg.method.unwrap(),
-            PyBytes::new(py, &msg.argvs.unwrap()));
+            PyBytes::new(py, &msg.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_call_hub_rpc", argvs) {
             error!("do_call_hub_rpc python callback error:{}", e)
         }
@@ -109,7 +134,9 @@ impl HubCallbackMsgHandle {
             hub_name,
             msg.entity_id.unwrap(), 
             msg.msg_cb_id.unwrap(), 
-            PyBytes::new(py, &msg.argvs.unwrap()));
+            PyBytes::new(py, &msg.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_call_hub_rsp", argvs) {
             error!("do_call_hub_rsp python callback error:{}", e)
         }
@@ -123,7 +150,9 @@ impl HubCallbackMsgHandle {
             hub_name,
             msg.entity_id.unwrap(), 
             msg.msg_cb_id.unwrap(), 
-            PyBytes::new(py, &msg.argvs.unwrap()));
+            PyBytes::new(py, &msg.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_call_hub_err", argvs) {
             error!("do_call_hub_err python callback error:{}", e)
         }
@@ -137,7 +166,9 @@ impl HubCallbackMsgHandle {
             hub_name,
             ev.entity_id.unwrap(), 
             msg.method.unwrap(), 
-            PyBytes::new(py, &msg.argvs.unwrap()));
+            PyBytes::new(py, &msg.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_call_hub_ntf", argvs) {
             error!("do_call_hub_ntf python callback error:{}", e)
         }
@@ -148,7 +179,9 @@ impl HubCallbackMsgHandle {
 
         let argvs = (
             hub_name,
-            ev.entity_id.unwrap());
+            ev.entity_id.unwrap()
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_wait_migrate_entity", argvs) {
             error!("do_wait_migrate_entity python callback error:{}", e)
         }
@@ -165,7 +198,9 @@ impl HubCallbackMsgHandle {
             ev.main_conn_id.unwrap(),
             ev.gates.unwrap(),
             ev.hubs.unwrap(),
-            PyBytes::new(py, &ev.argvs.unwrap()));
+            PyBytes::new(py, &ev.argvs.unwrap())
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_migrate_entity", argvs) {
             error!("do_migrate_entity python callback error:{}", e)
         }
@@ -177,7 +212,9 @@ impl HubCallbackMsgHandle {
         let argvs = (
             hub_name,
             ev.hub_name.unwrap(),
-            ev.entity_id.unwrap());
+            ev.entity_id.unwrap()
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_create_migrate_entity", argvs) {
             error!("do_create_migrate_entity python callback error:{}", e)
         }
@@ -189,7 +226,9 @@ impl HubCallbackMsgHandle {
         let argvs = (
             hub_name,
             ev.hub_name.unwrap(),
-            ev.entity_id.unwrap());
+            ev.entity_id.unwrap()
+        );
+
         if let Err(e) = py_handle.call_method1(py, "on_migrate_entity_complete", argvs) {
             error!("do_migrate_entity_complete python callback error:{}", e)
         }
