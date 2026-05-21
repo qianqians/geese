3977432925ee30823b531fba8849c04424d7b6fc
qianqians <451517996@qq.com>
2026-05-21 16:48:46 +0800

    Material

--- crates/render/src/lib.rs
+++ crates/render/src/lib.rs
@@ -1,23 +1,8 @@
-use cgmath::{Point3, Vector3, Vector2/*Matrix4, InnerSpace, EuclideanSpace, Rad, Deg, PerspectiveFov */};
+pub mod material;
+pub mod mesh;
 
-pub struct Vertex {
-    pub position: Point3<f32>,
-    pub normal: Vector3<f32>,
-    pub uv: Vector2<f32>,
-}
-
-pub struct ModelMesh {
-    pub vertices: Vec<Vertex>,
-    pub indices: Vec<u32>, 
-    pub material_index: usize,
-}
-
-impl ModelMesh {
-    pub fn new() -> Self {
-        ModelMesh {
-            vertices: Vec::new(),
-            indices: Vec::new(),
-            material_index: 0,
-        }
-    }
-}
+pub use material::{
+    AlphaMode, FilterMode, Material, MaterialHandle, MaterialLibrary, Sampler, Texture,
+    TextureFormat, TextureHandle, WrapMode,
+};
+pub use mesh::{ModelMesh, Vertex};
